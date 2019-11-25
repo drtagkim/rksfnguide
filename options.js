5e2f57af194bfcb5d7811df301ca66038d040879
DRTAG <masan.korea@gmail.com>
2019-11-25 13:19:27 +0900

    Add files via upload

--- options.js
+++ options.js
@@ -2,15 +2,18 @@
 const sd=document.querySelector("#sd");
 const ed=document.querySelector("#ed");
 const update=document.querySelector("#update");
-chrome.storage.sync.get(['startDate','endDate'],(data)=>{
+const wt=document.querySelector("#wt");
+chrome.storage.sync.get(['startDate','endDate','waiting'],(data)=>{
     sd.value=data.startDate;
     ed.value=data.endDate;
+    wt.value=Math.round(data.waiting/1000)
 });
 //
 update.addEventListener("click",(e)=>{
     chrome.storage.sync.set({
         startDate:sd.value,
-        endDate:ed.value
+        endDate:ed.value,
+        waiting:1000*parseInt(wt.value)
     },()=>{
         let msg=$("#msg");
         msg.text("Information updated.");
